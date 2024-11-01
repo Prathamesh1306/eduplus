6fc9e5699a8ab8d1e209fc72f135110a588cc05a
PriyaIngle7 <163649099+PriyaIngle7@users.noreply.github.com>
2024-11-01 11:42:34 +0530

    added student credential page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Student1 from "./Components/Page/Student/Student1.tsx";
 import Admin from "./Components/Page/Admin/admin.tsx";
 import Student2 from './Components/Page/Student/Studentverify.tsx'
+import StudentCredential from "./Components/Page/Student/Studentcredential";
+
+
 
 import AdminStudentList from "./Components/Page/Admin/adminStudentList.tsx";
 function App() {
@@ -14,6 +17,7 @@ function App() {
           <Route path='/student' element={<Student1 />} />
           <Route path='/admin-student-list' element={<AdminStudentList />} />
           <Route path='/student-verify' element={<Student2 />} />
+          <Route path="/studentcredential" element={<StudentCredential />} />
         </Routes>
       </div>  
     </Router>
---
+++ src/Components/Page/Student/Studentcredential.tsx
@@ -0,0 +1,28 @@
+import Header from "../../compo/header";
+import Footer from "../../compo/footer";
+import "../../css/studentcredential.css";
+
+
+function Studentcredential() {
+    return (
+        <div className="student-credential">
+            <Header name="Harsh" year="3rd year" role="STUDENT" />
+            
+            <div className="credential-content">
+                <h2>Credentials</h2>
+                
+                <div className="credential-card">
+                    <img src={ ' '} alt=" " className="id-card-image" />
+                </div>
+                
+                <div className="unique-id">
+                    <span>Unique ID:</span> 
+                </div>
+            </div>
+
+            <Footer />
+        </div>
+    );
+}
+
+export default Studentcredential;
