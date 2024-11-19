00cea0118b6887a903613ab906324b030fee52c1
harshmohite4 <mohiteharsh639@gmail.com>
2024-11-19 20:56:02 +0530

    updated

--- src/Components/Page/Landingpage.tsx
+++ src/Components/Page/Landingpage.tsx
@@ -14,8 +14,8 @@ function Landingpage() {
           flexDirection: "row",
           display: "flex",
           justifyContent: "space-around",
-          paddingLeft:30,
-          paddingRight:30
+          paddingLeft: 30,
+          paddingRight: 30,
         }}
       >
         <div style={{}}>
@@ -49,18 +49,18 @@ function Landingpage() {
           >
             Trusted Credentials, Verified in Seconds.
           </div>
-          <div >
+          <div>
             <div
               style={{
                 flexDirection: "row",
                 display: "flex",
                 alignItems: "center",
                 width: "60%",
-                marginTop: 40,
+                marginTop: 20,
               }}
             >
-              <Image12 size={360}/>
-              <div style={{ color: "#696984", width: "80%" ,fontSize:25}}>
+              <Image12 size={360} />
+              <div style={{ color: "#696984", width: "80%", fontSize: 25 }}>
                 Unbreakable Trust: Credentials are securely locked on the
                 blockchain, making them 100% tamper-proof for ultimate peace of
                 mind.
@@ -77,8 +77,8 @@ function Landingpage() {
                 justifyContent: "space-around",
               }}
             >
-              <Image13 size={360}/>
-              <div style={{ color: "#696984", width: "100%",fontSize:25 }}>
+              <Image13 size={360} />
+              <div style={{ color: "#696984", width: "100%", fontSize: 25 }}>
                 Lightning-Fast Verification: Recruiters can verify academic
                 records instantly, saving time and hiring costs.
               </div>
@@ -92,11 +92,10 @@ function Landingpage() {
                 width: "60%",
                 marginTop: 10,
                 justifyContent: "space-around",
-                
               }}
             >
               <Image14 size={360} />
-              <div style={{ color: "#696984", width: "80%",fontSize:25 }}>
+              <div style={{ color: "#696984", width: "80%", fontSize: 25 }}>
                 All-in-One Access: Students have everything in one place to
                 easily share with employers, while recruiters enjoy a smooth,
                 centralized verification process.
@@ -104,7 +103,40 @@ function Landingpage() {
             </div>
           </div>
         </div>
-        <Certificate size={1000} />
+        <div style={{ alignContent: "center" }}>
+          <Certificate size={500} />
+        </div>
+      </div>
+
+      <div
+        style={{
+          backgroundColor: "#0B7077",
+          marginTop: 10,
+          width: "30%",
+          justifySelf: "center",
+          paddingTop:20,
+          paddingBottom:20,
+          justifyItems:"center",
+          borderRadius:10
+        }}
+      >
+        <div style={{color:"#ffffff",fontSize:20}}>Sign up as Recruiter</div>
+        <div>
+
+        <input type="text" name="" id="" placeholder="Enter Email" />
+        </div>
+        <div>
+
+        <input type="text" name="" id="" placeholder="Enter Password" />
+        </div>
+
+        <div style={{color:"#000" ,backgroundColor:"#D2E6E4",paddingRight:10,paddingLeft:10,paddingTop:5,paddingBottom:5,borderRadius:15}}>
+            Sign in
+        </div>
+
+        <div style={{color:"#ffffff"}}>
+          sign in as <a href="/" style={{color:"#fff"}}>admin</a>   or <a href="" style={{color:"#fff"}}>recruiter</a> ? 
+        </div>
       </div>
 
       <Footer />
